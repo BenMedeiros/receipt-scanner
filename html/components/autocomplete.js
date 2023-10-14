7bb1f76880ed94fbde472990bfe09a332671a60f
Ben <ben.medeiros@planttherapy.com>
2023-10-13 20:55:52 -0600

    autocomplete respects tab/escape; hide elements that aren't matched using css

--- html/components/autocomplete.js
+++ html/components/autocomplete.js
@@ -1,14 +1,5 @@
 'use strict';
 
-// only one autocomplete component needs to exist for entire page
-
-let autocompleteEl = null;
-
-document.addEventListener('click', () => {
-  console.log('click');
-  closeAutocomplete();
-})
-
 export function bindAutocomplete(inputType, onclickCb) {
 //  document.activeElement
   inputType.element.addEventListener('input', (e) => {
@@ -18,22 +9,26 @@ export function bindAutocomplete(inputType, onclickCb) {
 }
 
 function rebuildAutoComplete(inputType, onclickCb) {
-  console.log('reubilduatocomepte');
-  if (autocompleteEl) autocompleteEl.remove();
+  // save who's autocomplete this is
+  if (inputType !== requesterInputType) requesterInputType = inputType;
 
-  autocompleteEl = document.createElement('ul');
-  autocompleteEl.id = 'autocomplete';
+  if (!autocompleteEl) {
+    autocompleteEl = document.createElement('ul');
+    autocompleteEl.id = 'autocomplete';
+    document.getElementById('main').appendChild(autocompleteEl);
+  }
+  clearAutocomplete();
   autocompleteEl.style.left = inputType.element.getBoundingClientRect().left + 'px';
   autocompleteEl.style.top = inputType.element.getBoundingClientRect().bottom + 'px';
 
   let currentValue = inputType.getValue();
-  if(currentValue) currentValue = currentValue.toUpperCase().trim();
+  if (currentValue) currentValue = currentValue.toUpperCase().trim();
 
   for (const value of Object.values(inputType.autocomplete)) {
     const li = document.createElement('li');
     //highlight autocomplete option with matches
     const matchIndex = value.toUpperCase().indexOf(currentValue);
-    console.log(value, currentValue, matchIndex);
+
     if (matchIndex === -1) {
       li.innerText = String(value);
     } else {
@@ -53,8 +48,9 @@ function rebuildAutoComplete(inputType, onclickCb) {
     li.onclick = () => autocompleteClicked(value, onclickCb);
     autocompleteEl.appendChild(li);
   }
-
-  document.getElementById('main').appendChild(autocompleteEl);
+  //unhide the autocomplete now that its built
+  autocompleteEl.style.visibility = 'initial';
+  listenForClose();
 }
 
 function autocompleteClicked(value, onclickCb) {
@@ -63,10 +59,43 @@ function autocompleteClicked(value, onclickCb) {
   closeAutocomplete();
 }
 
-function closeAutocomplete() {
+// check if the autocomplete should be closed based on key/click event
+function closeAutocomplete(event) {
   //remove and close the autocomplete
-  if (autocompleteEl) {
-    autocompleteEl.remove();
-    autocompleteEl = null;
+  if (autocompleteEl && autocompleteEl.style.visibility !== 'hidden') {
+    if (event && (event.code === 'Tab' || event.code === 'Escape')) {
+      //  if key down tab/escape, close even if in focus
+      // initially blank
+    } else if (document.activeElement === requesterInputType.element) {
+      // clicking on same element shouldn't close it
+      return;
+    }
+    autocompleteEl.style.visibility = 'hidden';
+    clearAutocomplete();
+    document.removeEventListener('click', closeAutocomplete);
+    document.addEventListener('input', closeAutocomplete);
+    listening = false;
   }
 }
+
+function clearAutocomplete() {
+  while (autocompleteEl.firstChild) {
+    autocompleteEl.removeChild(autocompleteEl.lastChild);
+  }
+}
+
+function listenForClose() {
+  // don't want to stack them
+  if (!listening) {
+    document.addEventListener('click', closeAutocomplete);
+    document.addEventListener('keydown', closeAutocomplete);
+    listening = true;
+  }
+}
+
+
+// only one autocomplete component needs to exist for entire page
+let autocompleteEl = null;
+let requesterInputType = null;
+let listening = false;
+
